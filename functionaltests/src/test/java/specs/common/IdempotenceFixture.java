d5ac29b8fd4602e751734084b3dbd17678611b3e
sesamzoo <sesamzoo@users.noreply.github.com>
2015-12-01 13:54:00 +0100

    Move files of functional tests to test folder.
    Make use of Gradle Java plugin conventions which looks for tests in the test folder.
    
    The main folder may be used for test support code like an application driver.

---
+++ functionaltests/src/test/java/specs/common/IdempotenceFixture.java
@@ -0,0 +1,10 @@
+package specs.common;
+
+import org.concordion.api.ExpectedToFail;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@ExpectedToFail
+@RunWith(ConcordionRunner.class)
+public class IdempotenceFixture {
+}
