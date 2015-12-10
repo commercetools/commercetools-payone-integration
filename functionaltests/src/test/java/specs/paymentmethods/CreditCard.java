6bedca8b7414d6faf981ca639628fb7dfbb003dd
Jan Wolter <jan.wolter@ecube-leipzig.de>
2015-12-10 08:55:46 +0100

    [WIP] Prepare fixture for credit card payments.

---
+++ functionaltests/src/test/java/specs/paymentmethods/CreditCard.java
@@ -0,0 +1,13 @@
+package specs.paymentmethods;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jan Wolter
+ */
+@Unimplemented
+@RunWith(ConcordionRunner.class)
+public class CreditCard {
+}
---
+++ functionaltests/src/test/java/specs/paymentmethods/PaymentMethods.java
@@ -0,0 +1,11 @@
+package specs.paymentmethods;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jan Wolter
+ */
+@RunWith(ConcordionRunner.class)
+public class PaymentMethods {
+}
