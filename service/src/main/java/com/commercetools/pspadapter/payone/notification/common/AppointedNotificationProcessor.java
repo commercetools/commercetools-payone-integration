57b7900070af3f91cbc4ce3e910cc0c5d478433d
Jan Wolter <jan.wolter@ecube-leipzig.de>
2016-01-15 14:19:35 +0100

    Extract methods and pull them to NotificationProcessorBase
    Reduce redundance.
    
    Contributes to #24.

--- service/src/main/java/com/commercetools/pspadapter/payone/notification/common/AppointedNotificationProcessor.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/notification/common/AppointedNotificationProcessor.java
@@ -1,34 +1,24 @@
 package com.commercetools.pspadapter.payone.notification.common;
 
 import com.commercetools.pspadapter.payone.domain.ctp.BlockingClient;
-import com.commercetools.pspadapter.payone.domain.ctp.CustomTypeBuilder;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.Notification;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.NotificationAction;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.TransactionStatus;
-import com.commercetools.pspadapter.payone.mapping.CustomFieldKeys;
 import com.commercetools.pspadapter.payone.notification.NotificationProcessorBase;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.payments.Payment;
 import io.sphere.sdk.payments.Transaction;
 import io.sphere.sdk.payments.TransactionDraftBuilder;
 import io.sphere.sdk.payments.TransactionState;
 import io.sphere.sdk.payments.TransactionType;
-import io.sphere.sdk.payments.commands.updateactions.AddInterfaceInteraction;
 import io.sphere.sdk.payments.commands.updateactions.AddTransaction;
 import io.sphere.sdk.payments.commands.updateactions.ChangeTransactionInteractionId;
 import io.sphere.sdk.payments.commands.updateactions.ChangeTransactionState;
 import io.sphere.sdk.utils.MoneyImpl;
 
 import javax.money.MonetaryAmount;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * An implementation of a NotificationProcessor specifically for notifications with txaction 'appointed'.
@@ -49,29 +39,21 @@ public class AppointedNotificationProcessor extends NotificationProcessorBase {
     }
 
     @Override
-    public NotificationAction supportedNotificationAction() {
+    protected NotificationAction supportedNotificationAction() {
         return NotificationAction.APPOINTED;
     }
 
     @Override
     protected ImmutableList<UpdateAction<Payment>> createPaymentUpdates(final Payment payment,
                                                                         final Notification notification) {
-        final LocalDateTime timestamp =
-                LocalDateTime.ofEpochSecond(Long.valueOf(notification.getTxtime()), 0, ZoneOffset.UTC);
+        final ImmutableList.Builder<UpdateAction<Payment>> listBuilder = ImmutableList.builder();
+
+        listBuilder.add(createNotificationAddAction(notification));
 
         final List<Transaction> transactions = payment.getTransactions();
-        final String sequenceNumber = Optional.ofNullable(notification.getSequencenumber()).orElse("0");
+        final String sequenceNumber = toSequenceNumber(notification.getSequencenumber());
 
-        final ImmutableList.Builder<UpdateAction<Payment>> listBuilder = ImmutableList.builder();
-        listBuilder.add(AddInterfaceInteraction.ofTypeKeyAndObjects(
-                CustomTypeBuilder.PAYONE_INTERACTION_NOTIFICATION,
-                ImmutableMap.of(
-                        CustomFieldKeys.TIMESTAMP_FIELD, ZonedDateTime.of(timestamp, ZoneId.of("UTC")),
-                        CustomFieldKeys.SEQUENCE_NUMBER_FIELD, sequenceNumber,
-                        CustomFieldKeys.TX_ACTION_FIELD, notification.getTxaction().getTxActionCode(),
-                        CustomFieldKeys.NOTIFICATION_FIELD, notification.toString())));
-
-        if (containsMatchingChargeTransaction(transactions, sequenceNumber)) {
+        if (findMatchingTransaction(transactions, TransactionType.CHARGE, sequenceNumber).isPresent()) {
             return listBuilder.build();
         }
 
@@ -85,7 +67,7 @@ public class AppointedNotificationProcessor extends NotificationProcessorBase {
 
                     //set transactionState if still pending and notification has status "complete"
                     if (t.getState().equals(TransactionState.PENDING) &&
-                            notification.getTransactionStatus().equals(TransactionStatus.COMPLETED)){
+                            notification.getTransactionStatus().equals(TransactionStatus.COMPLETED)) {
                         listBuilder.add(ChangeTransactionState
                                 .of(notification.getTransactionStatus().getCtTransactionState(), t.getId()));
                     }
@@ -97,29 +79,13 @@ public class AppointedNotificationProcessor extends NotificationProcessorBase {
                     final MonetaryAmount amount = MoneyImpl.of(notification.getPrice(), notification.getCurrency());
 
                     listBuilder.add(AddTransaction.of(TransactionDraftBuilder.of(TransactionType.AUTHORIZATION, amount)
-                            .timestamp(ZonedDateTime.of(timestamp, ZoneId.of("UTC")))
+                            .timestamp(toZonedDateTime(notification))
                             .state(notification.getTransactionStatus().getCtTransactionState())
                             .interactionId(notification.getSequencenumber())
                             .build()));
 
                     return listBuilder.build();
                 });
-    }
-
-    /**
-     * Checks whether {@code transactions} contains a {@link TransactionType#CHARGE} transaction with the
-     * {@code interactionId}.
-     *
-     * @param transactions the list of transactions
-     * @param interactionId the interaction ID (aka PAYONE's sequencenumber)
-     * @return whether there is a {@code CHARGE} transaction with the provided {@code interactionId} in
-     *         the {@code transactions}
-     */
-    private boolean containsMatchingChargeTransaction(final Collection<Transaction> transactions,
-                                                      final String interactionId) {
-        return transactions.stream().anyMatch(transaction ->
-                transaction.getType().equals(TransactionType.CHARGE)
-                        && Optional.ofNullable(transaction.getInteractionId()).orElse("0").equals(interactionId));
     }
 
 }
--- service/src/main/java/com/commercetools/pspadapter/payone/notification/common/CaptureNotificationProcessor.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/notification/common/CaptureNotificationProcessor.java
@@ -1,33 +1,21 @@
 package com.commercetools.pspadapter.payone.notification.common;
 
 import com.commercetools.pspadapter.payone.domain.ctp.BlockingClient;
-import com.commercetools.pspadapter.payone.domain.ctp.CustomTypeBuilder;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.Notification;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.NotificationAction;
-import com.commercetools.pspadapter.payone.mapping.CustomFieldKeys;
 import com.commercetools.pspadapter.payone.notification.NotificationProcessorBase;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.payments.Payment;
 import io.sphere.sdk.payments.Transaction;
 import io.sphere.sdk.payments.TransactionDraftBuilder;
 import io.sphere.sdk.payments.TransactionState;
 import io.sphere.sdk.payments.TransactionType;
-import io.sphere.sdk.payments.commands.updateactions.AddInterfaceInteraction;
 import io.sphere.sdk.payments.commands.updateactions.AddTransaction;
 import io.sphere.sdk.utils.MoneyImpl;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.money.MonetaryAmount;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * A NotificationProcessor for notifications with txaction "appointed".
@@ -46,68 +34,31 @@ public class CaptureNotificationProcessor extends NotificationProcessorBase {
     }
 
     @Override
+    protected NotificationAction supportedNotificationAction() {
+        return NotificationAction.CAPTURE;
+    }
+
+    @Override
     protected ImmutableList<UpdateAction<Payment>> createPaymentUpdates(final Payment payment,
                                                                         final Notification notification) {
-        final LocalDateTime timestamp =
-                LocalDateTime.ofEpochSecond(Long.valueOf(notification.getTxtime()), 0, ZoneOffset.UTC);
-
-        final String sequenceNumber = nullToZero(notification.getSequencenumber());
         final ImmutableList.Builder<UpdateAction<Payment>> listBuilder = ImmutableList.builder();
-        listBuilder.add(AddInterfaceInteraction.ofTypeKeyAndObjects(
-                CustomTypeBuilder.PAYONE_INTERACTION_NOTIFICATION,
-                ImmutableMap.of(
-                        CustomFieldKeys.TIMESTAMP_FIELD, ZonedDateTime.of(timestamp, ZoneId.of("UTC")),
-                        CustomFieldKeys.SEQUENCE_NUMBER_FIELD, sequenceNumber,
-                        CustomFieldKeys.TX_ACTION_FIELD, notification.getTxaction().getTxActionCode(),
-                        CustomFieldKeys.NOTIFICATION_FIELD, notification.toString())));
+        listBuilder.add(createNotificationAddAction(notification));
 
         final List<Transaction> transactions = payment.getTransactions();
+        final String sequenceNumber = toSequenceNumber(notification.getSequencenumber());
 
-        return findMatchingChargeTransaction(transactions, sequenceNumber)
+        return findMatchingTransaction(transactions, TransactionType.CHARGE, sequenceNumber)
                 .map(transaction -> listBuilder.build())
                 .orElseGet(() -> {
                     final MonetaryAmount amount = MoneyImpl.of(notification.getPrice(), notification.getCurrency());
 
                     listBuilder.add(AddTransaction.of(TransactionDraftBuilder.of(TransactionType.CHARGE, amount)
-                            .timestamp(ZonedDateTime.of(timestamp, ZoneId.of("UTC")))
+                            .timestamp(toZonedDateTime(notification))
                             .state(TransactionState.PENDING)
                             .interactionId(sequenceNumber)
                             .build()));
 
                     return listBuilder.build();
                 });
-    }
-
-    /**
-     * Checks whether {@code transactions} contains a {@link TransactionType#CHARGE} transaction with the
-     * {@code interactionId}.
-     *
-     * @param transactions the list of transactions
-     * @param interactionId the interaction ID (aka PAYONE's sequencenumber)
-     * @return whether there is a {@code CHARGE} transaction with the provided {@code interactionId} in
-     *         the {@code transactions}
-     */
-    private Optional<Transaction> findMatchingChargeTransaction(final Collection<Transaction> transactions,
-                                                                final String interactionId) {
-        return transactions.stream()
-                .filter(transaction ->
-                        transaction.getType().equals(TransactionType.CHARGE)
-                                && nullToZero(transaction.getInteractionId()).equals(interactionId))
-                .findFirst();
-    }
-
-    /**
-     * Transforms the {@code string} into "0" if necessary.
-     *
-     * @param string the string
-     * @return the {@code string} if not null, "0" otherwise
-     */
-    @Nonnull
-    private static String nullToZero(@Nullable final String string) {
-        return Optional.ofNullable(string).orElse("0");
-    }
-
-    public NotificationAction supportedNotificationAction() {
-        return NotificationAction.CAPTURE;
     }
 }
