e2ffdb25e9a8c070d81d27709010ada68047b3c7
Jan Wolter <jan.wolter@ecube-leipzig.de>
2016-01-21 15:43:03 +0100

    Add stubs for executable PayPal payments specification
    Contributes to #27 and #28.

---
+++ functionaltests/src/test/java/specs/paymentmethods/paypal/AuthorizationFixture.java
@@ -0,0 +1,13 @@
+package specs.paymentmethods.paypal;
+
+import org.concordion.api.ExpectedToFail;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jan Wolter
+ */
+@RunWith(ConcordionRunner.class)
+@ExpectedToFail
+public class AuthorizationFixture {
+}
---
+++ functionaltests/src/test/java/specs/paymentmethods/paypal/ChargeImmediatelyFixture.java
@@ -0,0 +1,13 @@
+package specs.paymentmethods.paypal;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jan Wolter
+ */
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class ChargeImmediatelyFixture {
+}
