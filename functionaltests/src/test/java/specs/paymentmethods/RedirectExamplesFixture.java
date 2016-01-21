b9b41c4718470edfcb236c5b849f9bdb617f834b
fhaertig <fhaertig@users.noreply.github.com>
2016-01-21 15:54:11 +0100

    prepare Fixture for full representation of redirect cases
    (Contribute to #23)

---
+++ functionaltests/src/test/java/specs/paymentmethods/RedirectExamplesFixture.java
@@ -0,0 +1,14 @@
+package specs.paymentmethods;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * @author fhaertig
+ * @date 21.01.16
+ */
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class RedirectExamplesFixture {
+}
