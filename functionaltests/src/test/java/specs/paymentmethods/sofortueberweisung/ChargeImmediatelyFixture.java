5a5eedbee61b090e2d2eced19b493cd3d2e582d6
fhaertig <fhaertig@users.noreply.github.com>
2016-01-22 17:15:58 +0100

    prepare Fixture for Charge Immediately with Sofortüberweisung

---
+++ functionaltests/src/test/java/specs/paymentmethods/sofortueberweisung/ChargeImmediatelyFixture.java
@@ -0,0 +1,15 @@
+package specs.paymentmethods.sofortueberweisung;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import specs.BaseFixture;
+
+/**
+ * @author fhaertig
+ * @since 22.01.16
+ */
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class ChargeImmediatelyFixture extends BaseFixture {
+}
---
+++ functionaltests/src/test/java/specs/paymentmethods/sofortueberweisung/SofortueberweisungFixture.java
@@ -0,0 +1,12 @@
+package specs.paymentmethods.sofortueberweisung;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * @author fhaertig
+ * @since 22.01.16
+ */
+@RunWith(ConcordionRunner.class)
+public class SofortueberweisungFixture {
+}
