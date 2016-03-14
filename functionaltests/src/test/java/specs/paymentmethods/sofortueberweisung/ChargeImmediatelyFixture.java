3cea5ebe8d9ada0363356e9d25cb9df84f7534f4
fhaertig <fhaertig@users.noreply.github.com>
2016-03-14 17:40:31 +0100

    add fixture implementation and web driver for Sofortuberweisung Charge Immediately; still expected to fail since we need to handle #68 correctly; further payment examples following
    (Contribute to #31)

--- functionaltests/src/test/java/specs/paymentmethods/sofortueberweisung/ChargeImmediatelyFixture.java
+++ functionaltests/src/test/java/specs/paymentmethods/sofortueberweisung/ChargeImmediatelyFixture.java
@@ -1,15 +1,232 @@
 package specs.paymentmethods.sofortueberweisung;
 
-import org.concordion.api.Unimplemented;
+import com.commercetools.pspadapter.payone.domain.ctp.CustomTypeBuilder;
+import com.commercetools.pspadapter.payone.mapping.CustomFieldKeys;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.sphere.sdk.client.BlockingSphereClient;
+import io.sphere.sdk.payments.Payment;
+import io.sphere.sdk.payments.PaymentDraft;
+import io.sphere.sdk.payments.PaymentDraftBuilder;
+import io.sphere.sdk.payments.PaymentMethodInfoBuilder;
+import io.sphere.sdk.payments.TransactionDraftBuilder;
+import io.sphere.sdk.payments.TransactionState;
+import io.sphere.sdk.payments.TransactionType;
+import io.sphere.sdk.payments.commands.PaymentCreateCommand;
+import io.sphere.sdk.payments.commands.PaymentUpdateCommand;
+import io.sphere.sdk.payments.commands.updateactions.AddTransaction;
+import io.sphere.sdk.types.CustomFieldsDraft;
+import org.apache.http.HttpResponse;
+import org.concordion.api.ExpectedToFail;
 import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import specs.BaseFixture;
+import util.WebDriverSofortueberweisung;
+
+import javax.money.MonetaryAmount;
+import javax.money.format.MonetaryFormats;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author fhaertig
  * @since 22.01.16
  */
 @RunWith(ConcordionRunner.class)
-@Unimplemented
+@ExpectedToFail
 public class ChargeImmediatelyFixture extends BaseFixture {
+    private static final Splitter thePaymentNamesSplitter = Splitter.on(", ");
+
+    private static final String baseRedirectUrl = "https://www.example.com/sofortueberweisung_charge_immediately/";
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChargeImmediatelyFixture.class);
+
+    private WebDriverSofortueberweisung webDriver;
+
+    private Map<String, String> successUrlForPayment;
+
+    @Before
+    public void setUp() {
+        webDriver = new WebDriverSofortueberweisung("12345", "12345");
+        successUrlForPayment = new HashMap<>();
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+
+    public String createPayment(final String paymentName,
+                                final String paymentMethod,
+                                final String transactionType,
+                                final String centAmount,
+                                final String currencyCode) throws ExecutionException, InterruptedException, UnsupportedEncodingException {
+
+
+        final MonetaryAmount monetaryAmount = createMonetaryAmountFromCent(Long.valueOf(centAmount), currencyCode);
+
+        final PaymentDraft paymentDraft = PaymentDraftBuilder.of(monetaryAmount)
+                .paymentMethodInfo(PaymentMethodInfoBuilder.of()
+                        .method(paymentMethod)
+                        .paymentInterface("PAYONE")
+                        .build())
+                .amountPlanned(monetaryAmount)
+                .custom(CustomFieldsDraft.ofTypeKeyAndObjects(
+                        CustomTypeBuilder.PAYMENT_BANK_TRANSFER,
+                        ImmutableMap.<String, Object>builder()
+                                .put(CustomFieldKeys.LANGUAGE_CODE_FIELD, Locale.ENGLISH.getLanguage())
+                                .put(CustomFieldKeys.SUCCESS_URL_FIELD, baseRedirectUrl + (paymentName + " Success").replace(" ", "-"))
+                                .put(CustomFieldKeys.ERROR_URL_FIELD, baseRedirectUrl + (paymentName + " Error").replace(" ", "-"))
+                                .put(CustomFieldKeys.CANCEL_URL_FIELD, baseRedirectUrl + (paymentName + " Cancel").replace(" ", "-"))
+                                .put(CustomFieldKeys.IBAN_FIELD, "DE85123456782599100003")
+                                .put(CustomFieldKeys.BIC_FIELD, "TESTTEST")
+                                .put(CustomFieldKeys.REFERENCE_FIELD, "myGlobalKey")
+                                .build()))
+                .build();
+
+        final BlockingSphereClient ctpClient = ctpClient();
+        final Payment payment = ctpClient.executeBlocking(PaymentCreateCommand.of(paymentDraft));
+        registerPaymentWithLegibleName(paymentName, payment);
+
+        createCartAndOrderForPayment(payment, currencyCode);
+
+        ctpClient.executeBlocking(PaymentUpdateCommand.of(
+                payment,
+                AddTransaction.of(TransactionDraftBuilder
+                        .of(TransactionType.valueOf(transactionType), monetaryAmount, ZonedDateTime.now())
+                        .state(TransactionState.PENDING)
+                        .build())));
+
+        return payment.getId();
+    }
+
+    public Map<String, String> handlePayment(final String paymentName,
+                                             final String requestType) throws ExecutionException, IOException {
+        final HttpResponse response = requestToHandlePaymentByLegibleName(paymentName);
+        final Payment payment = fetchPaymentByLegibleName(paymentName);
+        final String transactionId = getIdOfFirstTransaction(payment);
+        final String amountAuthorized = (payment.getAmountAuthorized() != null) ?
+                MonetaryFormats.getAmountFormat(Locale.GERMANY).format(payment.getAmountAuthorized()) :
+                BaseFixture.EMPTY_STRING;
+        final String amountPaid = (payment.getAmountPaid() != null) ?
+                MonetaryFormats.getAmountFormat(Locale.GERMANY).format(payment.getAmountPaid()) :
+                BaseFixture.EMPTY_STRING;
+
+        return ImmutableMap.<String, String> builder()
+                .put("statusCode", Integer.toString(response.getStatusLine().getStatusCode()))
+                .put("interactionCount", getInteractionRequestCountOverAllTransactions(payment, requestType))
+                .put("transactionState", getTransactionState(payment, transactionId))
+                .put("amountAuthorized", amountAuthorized)
+                .put("amountPaid", amountPaid)
+                .put("version", payment.getVersion().toString())
+                .build();
+    }
+
+    public Map<String, String> fetchPaymentDetails(final String paymentName)
+            throws InterruptedException, ExecutionException {
+        final Payment payment = fetchPaymentByLegibleName(paymentName);
+
+        final String transactionId = getIdOfFirstTransaction(payment);
+        final String responseRedirectUrl = Optional.ofNullable(payment.getCustom())
+                .flatMap(customFields ->
+                        Optional.ofNullable(customFields.getFieldAsString(CustomFieldKeys.REDIRECT_URL_FIELD)))
+                .orElse(NULL_STRING);
+
+        final int urlTrimAt = responseRedirectUrl.contains("?") ? responseRedirectUrl.indexOf("?") : 0;
+
+        final long appointedNotificationCount =
+                getInteractionNotificationCountOfAction(payment, "appointed", transactionId);
+
+        final long paidNotificationCount = getInteractionNotificationCountOfAction(payment, "paid", transactionId);
+
+        final String amountAuthorized = (payment.getAmountAuthorized() != null) ?
+                MonetaryFormats.getAmountFormat(Locale.GERMANY).format(payment.getAmountAuthorized()) :
+                NULL_STRING;
+
+        final String amountPaid = (payment.getAmountPaid() != null) ?
+                MonetaryFormats.getAmountFormat(Locale.GERMANY).format(payment.getAmountPaid()) :
+                BaseFixture.EMPTY_STRING;
+
+        return ImmutableMap.<String, String>builder()
+                .put("transactionState", getTransactionState(payment, transactionId))
+                .put("responseRedirectUrlStart", responseRedirectUrl.substring(0, urlTrimAt))
+                .put("responseRedirectUrlFull", responseRedirectUrl)
+                .put("successUrl", successUrlForPayment.getOrDefault(paymentName, EMPTY_STRING))
+                .put("amountAuthorized", amountAuthorized)
+                .put("amountPaid", amountPaid)
+                .put("appointedNotificationCount", String.valueOf(appointedNotificationCount))
+                .put("paidNotificationCount", String.valueOf(paidNotificationCount))
+                .put("version", payment.getVersion().toString())
+                .build();
+    }
+
+    public boolean executeRedirectForPayments(final String paymentNames) throws ExecutionException {
+        final Collection<String> paymentNamesList = ImmutableList.copyOf(thePaymentNamesSplitter.split(paymentNames));
+
+        paymentNamesList.forEach(paymentName -> {
+            final Payment payment = fetchPaymentByLegibleName(paymentName);
+            final Optional<String> responseRedirectUrl = Optional.ofNullable(payment.getCustom())
+                    .flatMap(customFields ->
+                            Optional.ofNullable(customFields.getFieldAsString(CustomFieldKeys.REDIRECT_URL_FIELD)));
+
+            if (responseRedirectUrl.isPresent()) {
+                final String successUrl =
+                        webDriver.executeSofortueberweisungRedirect(responseRedirectUrl.get(), getTestData3DsPassword())
+                                .replace(baseRedirectUrl, "[...]");
+
+                successUrlForPayment.put(paymentName, successUrl);
+            }
+        });
+
+        return successUrlForPayment.size() == paymentNamesList.size();
+    }
+
+
+    public boolean receivedNotificationOfActionFor(final String paymentNames, final String txaction) throws InterruptedException, ExecutionException {
+        final ImmutableList<String> paymentNamesList = ImmutableList.copyOf(thePaymentNamesSplitter.split(paymentNames));
+
+        long remainingWaitTimeInMillis = PAYONE_NOTIFICATION_TIMEOUT;
+
+        final long sleepDuration = 100L;
+
+        long numberOfPaymentsWithNotification = countPaymentsWithNotificationOfAction(paymentNamesList, txaction);
+        while ((numberOfPaymentsWithNotification != paymentNamesList.size()) && (remainingWaitTimeInMillis > 0L)) {
+            Thread.sleep(sleepDuration);
+            remainingWaitTimeInMillis -= sleepDuration;
+            numberOfPaymentsWithNotification = countPaymentsWithNotificationOfAction(paymentNamesList, txaction);
+            if (remainingWaitTimeInMillis == TimeUnit.MINUTES.toMillis(4)
+                    || remainingWaitTimeInMillis == TimeUnit.MINUTES.toMillis(2)) {
+                LOG.info("Waiting for " + txaction + " notifications in ChargedImmediatelyWith3dsFixture takes longer than usual.");
+            }
+        }
+
+        LOG.info(String.format(
+                "waited %d seconds to receive notifications of type '%s' for payments %s",
+                TimeUnit.MILLISECONDS.toSeconds(PAYONE_NOTIFICATION_TIMEOUT - remainingWaitTimeInMillis),
+                txaction,
+                Arrays.toString(paymentNamesList.stream().map(this::getIdForLegibleName).toArray())));
+
+        return numberOfPaymentsWithNotification == paymentNamesList.size();
+    }
+
+    public boolean isInteractionRedirectPresent(final String paymentName) throws ExecutionException {
+        Payment payment = fetchPaymentByLegibleName(paymentName);
+        final String transactionId = getIdOfLastTransaction(payment);
+
+        return getInteractionRedirect(payment, transactionId).isPresent();
+    }
 }
---
+++ functionaltests/src/test/java/util/WebDriverSofortueberweisung.java
@@ -0,0 +1,112 @@
+package util;
+
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.not;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author fhaertig
+ * @since 14.03.16
+ */
+public class WebDriverSofortueberweisung extends HtmlUnitDriver {
+
+    private static final int DEFAULT_TIMEOUT = 5;
+
+    private final String pin;
+
+    private final String tan;
+
+    public WebDriverSofortueberweisung(final String pin, final String tan) {
+        super(BrowserVersion.FIREFOX_38, true);
+
+        this.pin = pin;
+        this.tan = tan;
+
+        final Timeouts timeouts = manage().timeouts();
+        timeouts.implicitlyWait(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+        timeouts.pageLoadTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+        timeouts.setScriptTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+
+        final WebClient webClient = getWebClient();
+        webClient.setJavaScriptTimeout(2000);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setPopupBlockerEnabled(true);
+
+        webClient.setIncorrectnessListener((message, origin) -> {
+            //swallow these messages
+        });
+        webClient.setCssErrorHandler(new SilentCssErrorHandler());
+    }
+
+    private void doLogin(final String userid) {
+        final WebElement useridInput = findElement(By.id("BackendFormLOGINNAMEUSERID"));
+        final WebElement pinInput = findElement(By.id("BackendFormUSERPIN"));
+        final WebElement submitButton = findElement(By.cssSelector("button[type=submit]"));
+
+        useridInput.clear();
+        useridInput.sendKeys(userid);
+        pinInput.sendKeys(pin);
+        submitButton.click();
+    }
+
+
+    private void selectAccount() {
+        final WebElement senderAccountInput = findElement(By.id("TransactionsSessionSenderAccountNumber23456789"));
+        final WebElement submitButton = findElement(By.cssSelector("button[type=submit]"));
+
+        senderAccountInput.click();
+        submitButton.click();
+    }
+
+    private void provideTan() {
+        final WebElement tanInput = findElement(By.id("BackendFormTan"));
+        final WebElement submitButton = findElement(By.cssSelector("button[type=submit]"));
+
+        tanInput.sendKeys(tan);
+        submitButton.click();
+    }
+
+    /**
+     * Submits the given {@code password} at the given {@code url}'s "password" element, waits for a redirect and
+     * returns the URL it was redirected to.
+     *
+     * @param url the URL to navigate to
+     * @param userid the account id to use
+     * @return the URL the browser was redirected to after submitting the {@code password}
+     */
+    public String executeSofortueberweisungRedirect(final String url, final String userid) {
+        final Wait<WebDriver> wait = new WebDriverWait(this, 10);
+
+        navigate().to(url);
+
+        doLogin(userid);
+
+        wait.until(ExpectedConditions.urlContains("select_account"));
+
+        selectAccount();
+
+        wait.until(ExpectedConditions.urlContains("provide_tan"));
+
+        provideTan();
+
+        wait.until(not(ExpectedConditions.urlContains("pay1")));
+
+        return getCurrentUrl();
+    }
+
+    public void quit() {
+        super.quit();
+    }
+}
