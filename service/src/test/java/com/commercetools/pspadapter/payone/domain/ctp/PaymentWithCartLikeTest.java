6134a3e185050efce83bfc29af6f0baabfa073b5
fhaertig <fhaertig@users.noreply.github.com>
2016-03-17 14:51:53 +0100

    fix test for PaymentWithCartLike: check for IllegalStateException
    (contribute to #51)

--- service/src/test/java/com/commercetools/pspadapter/payone/domain/ctp/PaymentWithCartLikeTest.java
+++ service/src/test/java/com/commercetools/pspadapter/payone/domain/ctp/PaymentWithCartLikeTest.java
@@ -1,11 +1,13 @@
 package com.commercetools.pspadapter.payone.domain.ctp;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
 
 import com.commercetools.pspadapter.payone.mapping.CustomFieldKeys;
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.orders.Order;
 import io.sphere.sdk.payments.Payment;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import util.PaymentTestHelper;
 
@@ -33,6 +35,7 @@ public class PaymentWithCartLikeTest {
         Payment payment = testHelper.dummyPaymentOneAuthPending20EuroCC();
         Cart cart = testHelper.dummyCart();
 
+
         PaymentWithCartLike testee = new PaymentWithCartLike(payment, cart);
 
         assertThat(testee.getOrderNumber()).isEqualTo(payment.getCustom().getFieldAsString(CustomFieldKeys.REFERENCE_FIELD));
@@ -44,20 +47,21 @@ public class PaymentWithCartLikeTest {
         Payment payment = testHelper.dummyPaymentNoCustomFields();
         Order order = testHelper.dummyOrderMapToPayoneRequest();
 
-        PaymentWithCartLike testee = new PaymentWithCartLike(payment, order);
+        final Throwable throwable = catchThrowable(() -> new PaymentWithCartLike(payment, order));
 
-        assertThat(testee.getOrderNumber()).isEqualTo(null);
-        assertThat(testee.getCartLike()).isEqualTo(order);
+        Assertions.assertThat(throwable)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("is missing the required custom field");
     }
 
     @Test
     public void createWithNewPaymentOrderNumberChanges() throws Exception {
-        Payment payment = testHelper.dummyPaymentNoCustomFields();
+        Payment payment = testHelper.dummyPaymentOneAuthPending20EuroPNT();
         Order order = testHelper.dummyOrderMapToPayoneRequest();
 
         PaymentWithCartLike testee = new PaymentWithCartLike(payment, order);
 
-        assertThat(testee.getOrderNumber()).isEqualTo(null);
+        assertThat(testee.getOrderNumber()).isEqualTo(payment.getCustom().getFieldAsString(CustomFieldKeys.REFERENCE_FIELD));
         assertThat(testee.getCartLike()).isEqualTo(order);
 
         Payment newPayment = testHelper.dummyPaymentOneAuthPending20EuroCC();
