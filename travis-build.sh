09fb87417aaf8d98484563238ac6422b7c6bc2ac
Sven Mueller <sven.mueller@commercetools.de>
2016-08-30 12:38:05 +0200

    Automate Docker image build/push process.

---
+++ travis-build.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -e
+
+export REPO="sphereio/commercetools-payone-integration"
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then echo "latest"; else echo ${TRAVIS_BRANCH/\//-} ; fi`
+
+echo "Building service jar file including all dependencies."
+./gradlew stage
+
+echo "Building Docker image using tag '${REPO}:${COMMIT}'."
+docker build -t "${REPO}:${COMMIT}" .
+
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+
+echo "Adding additional tag '${REPO}:${TAG}' to already built Docker image '${REPO}:${COMMIT}'."
+docker tag $REPO:$COMMIT $REPO:$TAG
+echo "Adding additional tag '${REPO}:travis-${TRAVIS_BUILD_NUMBER}' to already built Docker image '${REPO}:${COMMIT}'."
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+if [ "$TRAVIS_TAG" ]; then
+  echo "Adding additional tag '${REPO}:${TRAVIS_TAG}' to already built Docker image '${REPO}:${COMMIT}'."
+  docker tag $REPO:$COMMIT $REPO:${TRAVIS_TAG};
+fi
+echo "Pushing Docker images to repository '${REPO}' (all local tags are pushed)."
+docker push $REPO
+docker logout
