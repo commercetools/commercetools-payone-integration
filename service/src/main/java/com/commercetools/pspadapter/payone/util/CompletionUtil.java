a70d2ea79eac1c5d625f63e9d66bcf8273d67d0b
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-03-20 12:15:51 +0100

    #139: update CompletionUtil#executeBlocking() doc

--- service/src/main/java/com/commercetools/pspadapter/payone/util/CompletionUtil.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/util/CompletionUtil.java
@@ -1,14 +1,25 @@
 package com.commercetools.pspadapter.payone.util;
 
 import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 public final class CompletionUtil {
 
     /**
-     * This method us used as an intermediate step of re-factoring blocking Sphere client usage to use stages,
+     * This method is used as an intermediate step of re-factoring blocking Sphere client usage to use stages,
      * but in some places blocking operation are still required.
      * <p>
-     * <b>Note:</b> usage of this method should be avoided as much as possible!
+     * <b>Note:</b> usage of this method should be avoided as much as possible! At least never use a sequence of
+     * {@code executeBlocking(CompletionStage<T>)}, like:
+     * <pre>
+     *  Payment payment = executeBlocking(paymentStage);
+     *  Order order     = executeBlocking(orderService.getOrderForPayment(payment));
+     * </pre>
+     * Instead use {@link CompletionStage#thenComposeAsync(Function)}:
+     * <pre>
+     *  Order order = executeBlocking(paymentStage
+     *                .thenComposeAsync(payment -> orderService.getOrderForPayment(payment)));
+     * </pre>
      *
      * @param completionStage {@link CompletionStage} to wait blocking.
      * @param <T>
