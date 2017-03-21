636a8e96c0743eb2ffd6b24b07569f9cdba7d327
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-03-21 14:15:30 +0100

    #139: re-factor string literals to appropriate Enum values
    in Transaction Executors response parsing

--- service/src/main/java/com/commercetools/pspadapter/payone/transaction/common/DefaultChargeTransactionExecutor.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/transaction/common/DefaultChargeTransactionExecutor.java
@@ -5,6 +5,7 @@ import com.commercetools.pspadapter.payone.domain.ctp.PaymentWithCartLike;
 import com.commercetools.pspadapter.payone.domain.payone.PayonePostService;
 import com.commercetools.pspadapter.payone.domain.payone.exceptions.PayoneException;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.AuthorizationRequest;
+import com.commercetools.pspadapter.payone.domain.payone.model.common.ResponseStatus;
 import com.commercetools.pspadapter.payone.mapping.CustomFieldKeys;
 import com.commercetools.pspadapter.payone.mapping.PayoneRequestFactory;
 import com.commercetools.pspadapter.payone.transaction.TransactionBaseExecutor;
@@ -110,7 +111,7 @@ public class DefaultChargeTransactionExecutor extends TransactionBaseExecutor {
             final Map<String, String> response = payonePostService.executePost(request);
 
             final String status = response.get("status");
-            if (status.equals("REDIRECT")) {
+            if (ResponseStatus.REDIRECT.getStateCode().equals(status)) {
                 final AddInterfaceInteraction interfaceInteraction = AddInterfaceInteraction.ofTypeKeyAndObjects(CustomTypeBuilder.PAYONE_INTERACTION_REDIRECT,
                     ImmutableMap.of(CustomFieldKeys.RESPONSE_FIELD, responseToJsonString(response),
                             CustomFieldKeys.REDIRECT_URL_FIELD, response.get("redirecturl"),
@@ -128,7 +129,7 @@ public class DefaultChargeTransactionExecutor extends TransactionBaseExecutor {
                             CustomFieldKeys.TRANSACTION_ID_FIELD, transaction.getId(),
                             CustomFieldKeys.TIMESTAMP_FIELD, ZonedDateTime.now() /* TODO */));
 
-                if (status.equals("APPROVED")) {
+                if (ResponseStatus.APPROVED.getStateCode().equals(status)) {
 
                     return update(paymentWithCartLike, updatedPayment, ImmutableList.of(
                             interfaceInteraction,
@@ -139,7 +140,7 @@ public class DefaultChargeTransactionExecutor extends TransactionBaseExecutor {
                             ChangeTransactionState.of(TransactionState.SUCCESS, transaction.getId()),
                             ChangeTransactionTimestamp.of(ZonedDateTime.now(), transaction.getId())
                     ));
-                } else if (status.equals("ERROR")) {
+                } else if (ResponseStatus.ERROR.getStateCode().equals(status)) {
                     return update(paymentWithCartLike, updatedPayment, ImmutableList.of(
                             interfaceInteraction,
                             setStatusInterfaceCode(response),
@@ -147,7 +148,7 @@ public class DefaultChargeTransactionExecutor extends TransactionBaseExecutor {
                             ChangeTransactionState.of(TransactionState.FAILURE, transaction.getId()),
                             ChangeTransactionTimestamp.of(ZonedDateTime.now(), transaction.getId())
                     ));
-                } else if (status.equals("PENDING")) {
+                } else if (ResponseStatus.PENDING.getStateCode().equals(status)) {
                     return update(paymentWithCartLike, updatedPayment, ImmutableList.of(
                             interfaceInteraction,
                             setStatusInterfaceCode(response),
