17da0f6ed1c4b4b1e0b6d561fe03d8cfa3c0dc38
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-05-26 19:05:20 +0200

    #155: first Klarna functional test implemented

---
+++ functionaltests/src/test/java/specs/paymentmethods/klarna/KlarnaRequest.java
@@ -0,0 +1,59 @@
+package specs.paymentmethods.klarna;
+
+import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.orders.Order;
+import io.sphere.sdk.payments.Payment;
+import io.sphere.sdk.types.CustomFields;
+import io.sphere.sdk.utils.MoneyImpl;
+import org.apache.http.HttpResponse;
+import org.concordion.api.MultiValueResult;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import specs.response.BasePaymentFixture;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static com.commercetools.pspadapter.payone.mapping.CustomFieldKeys.IP;
+import static java.util.Optional.ofNullable;
+
+@RunWith(ConcordionRunner.class)
+public class KlarnaRequest extends BasePaymentFixture {
+
+    public MultiValueResult createPayment(
+            final String paymentName,
+            final String transactionType,
+            final String ip) throws Exception {
+
+        Cart cart = createTemplateCartKlarna();
+        cart = applyDiscounts(cart, DISCOUNT_999_CENT, DISCOUNT_10_PERCENT);
+        Order order = createOrderForCart(cart);
+
+        Payment payment = createAndSaveKlarnaPayment(cart, order, paymentName, transactionType,
+                MoneyImpl.centAmountOf(cart.getTotalPrice()).toString(),
+                cart.getTotalPrice().getCurrency().getCurrencyCode(),
+                ip);
+
+        Optional<CustomFields> custom = ofNullable(payment.getCustom());
+
+        return MultiValueResult.multiValueResult()
+                .with("paymentId", payment.getId())
+                .with("ip", custom.map(customFields -> customFields.getFieldAsString(IP)).orElse("undefined"));
+    }
+
+    public MultiValueResult handlePayment(final String paymentName,
+                                          final String requestType) throws ExecutionException, IOException {
+        final HttpResponse response = requestToHandlePaymentByLegibleName(paymentName);
+        final Payment payment = fetchPaymentByLegibleName(paymentName);
+        final String transactionId = getIdOfLastTransaction(payment);
+
+        return MultiValueResult.multiValueResult()
+                .with("statusCode", Integer.toString(response.getStatusLine().getStatusCode()))
+                .with("interactionCount", getInteractionRequestCount(payment, transactionId, requestType))
+                .with("transactionState", getTransactionState(payment, transactionId))
+                .with("version", payment.getVersion().toString())
+                .with("interfaceCode", payment.getPaymentStatus().getInterfaceCode());
+    }
+
+}
--- functionaltests/src/test/java/specs/response/BasePaymentFixture.java
+++ functionaltests/src/test/java/specs/response/BasePaymentFixture.java
@@ -1,6 +1,7 @@
 package specs.response;
 
 import com.commercetools.pspadapter.payone.domain.ctp.CustomTypeBuilder;
+import com.commercetools.pspadapter.payone.domain.ctp.paymentmethods.PaymentMethod;
 import com.commercetools.pspadapter.payone.mapping.CustomFieldKeys;
 import com.commercetools.service.OrderService;
 import com.commercetools.service.OrderServiceImpl;
@@ -8,9 +9,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import io.sphere.sdk.client.BlockingSphereClient;
+import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.carts.CartDraft;
+import io.sphere.sdk.carts.commands.CartCreateCommand;
+import io.sphere.sdk.carts.commands.CartUpdateCommand;
+import io.sphere.sdk.carts.commands.updateactions.AddDiscountCode;
+import io.sphere.sdk.carts.commands.updateactions.AddPayment;
+import io.sphere.sdk.carts.queries.CartByIdGet;
 import io.sphere.sdk.commands.UpdateActionImpl;
+import io.sphere.sdk.customers.Customer;
+import io.sphere.sdk.customers.queries.CustomerQueryBuilder;
 import io.sphere.sdk.json.SphereJsonUtils;
+import io.sphere.sdk.orders.Order;
+import io.sphere.sdk.orders.OrderFromCartDraft;
+import io.sphere.sdk.orders.commands.OrderFromCartCreateCommand;
 import io.sphere.sdk.payments.*;
 import io.sphere.sdk.payments.commands.PaymentCreateCommand;
 import io.sphere.sdk.payments.commands.PaymentUpdateCommand;
@@ -20,31 +32,37 @@ import io.sphere.sdk.types.CustomFieldsDraft;
 import model.HandlePaymentResult;
 import org.apache.http.HttpResponse;
 import org.junit.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import specs.BaseFixture;
 
 import javax.money.MonetaryAmount;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import static com.commercetools.pspadapter.payone.domain.ctp.paymentmethods.MethodKeys.*;
+import static com.commercetools.pspadapter.payone.util.CompletionUtil.executeBlocking;
 import static io.sphere.sdk.payments.TransactionState.FAILURE;
 import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+import static java.util.Optional.ofNullable;
 
 /**
  * Base class to create and handle test payments for Payone.
  */
 public class BasePaymentFixture extends BaseFixture {
 
-    private static final Logger LOG = LoggerFactory.getLogger(BasePaymentFixture.class);
-
     public static final String baseRedirectUrl = "https://www.example.com/sofortueberweisung_charge_immediately/";
+
+    public static final String DISCOUNT_10_PERCENT = "test-10-percent-code";
+    public static final String DISCOUNT_999_CENT = "test-999-cent-code";
+
+    public static final String KLARNA_DEFAULT_CART_MOCK = "mocks/paymentmethods/klarna/KlarnaCartWithTestAccountAddress.json";
 
     protected OrderService orderService;
 
@@ -182,6 +200,68 @@ public class BasePaymentFixture extends BaseFixture {
     }
 
     /**
+     * Create Klarna payment and attach to the {@code cart}.
+     * The request is created form cart JSON mock + CTP user with email youremail@email.com
+     * See <a href="https://developers.klarna.com/en/de/kpm/test-credentials">Klarna Test credentials</a>.
+     *
+     * So far the language ("de") and gender ("m") are hardcoded.
+     * @return new payment reference with respective {@code transactionType}, and attached to cart.
+     */
+    public Payment createAndSaveKlarnaPayment(
+            final Cart cart,
+            final Order order,
+            final String paymentName,
+            final String transactionType,
+            final String centAmount,
+            final String currencyCode,
+            final String ip) {
+        final MonetaryAmount monetaryAmount = createMonetaryAmountFromCent(Long.valueOf(centAmount), currencyCode);
+
+        // customer is required to fetch mandatory Klarna fields, like date of birth
+        Customer customer = ctpClient().executeBlocking(CustomerQueryBuilder.of()
+                .predicates(customerModel -> customerModel.lowercaseEmail().is("youremail@email.com"))
+                .build())
+                .head().orElseThrow(() -> new IllegalStateException("Customer youremail@email.com for klarna test payment has not found"));
+
+        final PaymentDraft paymentDraft = PaymentDraftBuilder.of(monetaryAmount)
+                .paymentMethodInfo(PaymentMethodInfoBuilder.of()
+                        .method(PaymentMethod.INVOICE_KLARNA.getKey())
+                        .paymentInterface("PAYONE")
+                        .build())
+                .transactions(singletonList(TransactionDraftBuilder.of(
+                        TransactionType.valueOf(transactionType),
+                        monetaryAmount,
+                        ZonedDateTime.now())
+                        .state(TransactionState.PENDING)
+                        .build()))
+                .customer(customer)
+                .custom(CustomFieldsDraft.ofTypeKeyAndObjects(
+                        CustomTypeBuilder.PAYMENT_INVOICE_KLARNA,
+                        ImmutableMap.<String, Object>builder()
+                                .put(CustomFieldKeys.LANGUAGE_CODE_FIELD,
+                                        ofNullable(cart.getLocale())
+                                                .map(Locale::getLanguage)
+                                                .orElseGet(Locale.GERMAN::getLanguage))
+                                .put(CustomFieldKeys.REFERENCE_FIELD, ofNullable(order.getOrderNumber())
+                                        .orElseThrow(() -> new IllegalStateException("Order must have a number")))
+                                .put(CustomFieldKeys.IP, ip)
+                                .put(CustomFieldKeys.GENDER_FIELD, "m")
+                                .build()))
+                .build();
+
+        final Payment payment = ctpClient().executeBlocking(PaymentCreateCommand.of(paymentDraft));
+
+        registerPaymentWithLegibleName(paymentName, payment);
+
+        // set payment to the Cart#paymentInfo
+        executeBlocking(ctpClient().execute(CartByIdGet.of(cart))
+                .thenCompose(newCart -> ctpClient().execute(CartUpdateCommand.of(newCart, AddPayment.of(payment)))));
+
+        return payment;
+
+    }
+
+    /**
      * Creates and saves payment from supplied draft. The method may be reused for different payment methods and types
      * as soon as they are already set in {@code paymentDraft}.
      * @param paymentName Unique payment name to use inside this test fixture
@@ -190,14 +270,13 @@ public class BasePaymentFixture extends BaseFixture {
      * @return Instance of created and saved payment object
      */
     protected Payment createPaymentCartOrderFromDraft(String paymentName, PaymentDraft paymentDraft, String transactionType, String buyerLastName) {
-        final BlockingSphereClient ctpClient = ctpClient();
-        final Payment payment = ctpClient.executeBlocking(PaymentCreateCommand.of(paymentDraft));
+        final Payment payment = ctpClient().executeBlocking(PaymentCreateCommand.of(paymentDraft));
 
         registerPaymentWithLegibleName(paymentName, payment);
 
         final String orderNumber = createCartAndOrderForPayment(payment, paymentDraft.getAmountPlanned().getCurrency().getCurrencyCode(), buyerLastName);
 
-        ctpClient.executeBlocking(PaymentUpdateCommand.of(
+        ctpClient().executeBlocking(PaymentUpdateCommand.of(
                 payment,
                 ImmutableList.<UpdateActionImpl<Payment>>builder()
                         .add(AddTransaction.of(TransactionDraftBuilder.of(
@@ -215,6 +294,35 @@ public class BasePaymentFixture extends BaseFixture {
     protected Payment createPaymentCartOrderFromDraft(String paymentName, PaymentDraft paymentDraft, String transactionType) {
         return createPaymentCartOrderFromDraft(paymentName, paymentDraft, transactionType, BUYER_LAST_NAME);
     }
+
+    /**
+     * Create a cart ready for Klarna payment. This cart expected to have multiple line items,
+     * shipping/billing address is respective to Klarna test data requirements.
+     */
+    public Cart createTemplateCartKlarna() {
+        CartDraft cardDraft = SphereJsonUtils.readObjectFromResource(KLARNA_DEFAULT_CART_MOCK, CartDraft.class);
+        return ctpClient().executeBlocking(CartCreateCommand.of(cardDraft));
+    }
+
+    public Order createOrderForCart(Cart cart) {
+        String orderNumber = getRandomOrderNumber();
+        return ctpClient().executeBlocking(OrderFromCartCreateCommand.of(
+                OrderFromCartDraft.of(cart, orderNumber, null)));
+    }
+
+    /**
+     * Apply {@code discountCodes} to the cart. The discounts are applied in the order they appear in the list.
+     * @param cart cart to update
+     * @param discountCodes  codes to apply to the {@code cart}
+     * @return the same cart, but with updated prices.
+     */
+    public Cart applyDiscounts(Cart cart, String ...discountCodes) {
+        return ctpClient().executeBlocking(CartUpdateCommand.of(cart,
+                Arrays.stream(discountCodes)
+                        .map(AddDiscountCode::of)
+                        .collect(Collectors.toList())));
+    }
+
 
     /**
      * Handle response by given name and parse "response" result from JSON string to {@link JsonNode} result.
@@ -241,7 +349,7 @@ public class BasePaymentFixture extends BaseFixture {
      */
     protected void validatePaymentsNotFailed(final List<String> paymentNamesList) throws IllegalStateException {
         // since fetchPaymentByLegibleName() is a slow blocking operation - make the stream parallel
-        paymentNamesList.parallelStream().forEach(paymentName ->{
+        paymentNamesList.parallelStream().forEach(paymentName -> {
             Payment payment = fetchPaymentByLegibleName(paymentName);
             List<Transaction> transactions = payment.getTransactions();
             TransactionState lastTransactionState = transactions.size() > 0 ? transactions.get(transactions.size() - 1).getState() : null;
