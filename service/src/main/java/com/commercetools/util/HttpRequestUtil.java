f5e8d81fe8100d15bced368b09934f7067f7358a
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2017-11-07 14:20:22 +0100

    #185: revert socket timeout

--- service/src/main/java/com/commercetools/util/HttpRequestUtil.java
+++ service/src/main/java/com/commercetools/util/HttpRequestUtil.java
@@ -82,7 +82,7 @@ public final class HttpRequestUtil {
     private static final CloseableHttpClient CLIENT = HttpClientBuilder.create()
             .setDefaultRequestConfig(RequestConfig.custom()
                     .setConnectionRequestTimeout(REQUEST_TIMEOUT)
-                    .setSocketTimeout(REQUEST_TIMEOUT * 6)
+                    .setSocketTimeout(REQUEST_TIMEOUT)
                     .setConnectTimeout(REQUEST_TIMEOUT)
                     .build())
             .setRetryHandler(HTTP_REQUEST_RETRY_ON_SOCKET_TIMEOUT)
