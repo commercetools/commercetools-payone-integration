2925c7a15e5ceffc7777327ca04c34e598aaafdb
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2017-11-07 11:05:13 +0100

    #177: comments

--- service/src/main/java/com/commercetools/pspadapter/payone/util/PayoneHash.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/util/PayoneHash.java
@@ -9,12 +9,12 @@ public final class PayoneHash {
     /**
      * Calculate MD5 hash sum of a given input string
      * <p>
-     * <b>Note:</b> Currently Payone doesn't support SHA2. We should change the hashing method
-     * after they introduce support for SHA2 since MD5 hashing counts as deprecated
-     * </p>
+     * <b>Note:</b> Currently Payone <b>server</b> API (opposite to client API) doesn't support SHA2.
+     * We should change the hashing method as soon as they introduce support for SHA2 on server API.
+     *
      * @param in from which to calculate the hash
      * @return MD5 hash string
-     * */
+     */
     @SuppressWarnings("deprecation")
     public static String calculate(String in) {
         return Hashing.md5().hashString(in, Charsets.UTF_8).toString();
