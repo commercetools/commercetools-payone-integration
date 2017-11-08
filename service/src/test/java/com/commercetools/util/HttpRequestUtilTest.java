9cec14d379efd041156776edab603dc54f549023
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2017-11-08 14:17:32 +0100

    #191: move HttpRequestUtil tests from functional tests to service unit tests

--- service/src/test/java/com/commercetools/util/HttpRequestUtilTest.java
+++ service/src/test/java/com/commercetools/util/HttpRequestUtilTest.java
@@ -1,28 +1,32 @@
 package com.commercetools.util;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import io.sphere.sdk.http.HttpStatusCode;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static com.commercetools.util.HttpRequestUtil.*;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static java.util.Optional.ofNullable;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.MapEntry.entry;
 
 public class HttpRequestUtilTest {
 
     private static final String HTTP_HTTPBIN_ORG_GET = "http://httpbin.org/get";
     private static final String HTTP_HTTPBIN_ORG_POST = "http://httpbin.org/post";
 
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
+
     @Test
     public void executeGetRequest_isSuccessful() throws Exception {
         HttpResponse response = executeGetRequest(HTTP_HTTPBIN_ORG_GET);
-        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
-    }
-
-    @Test
-    public void executePostRequest_isSuccessful() throws Exception {
-        HttpResponse response = executePostRequest(HTTP_HTTPBIN_ORG_POST, singletonList(nameValue("a", "b")));
         assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
     }
 
@@ -32,10 +36,47 @@ public class HttpRequestUtilTest {
     }
 
     @Test
+    public void executePostRequest_isSuccessful() throws Exception {
+        HttpResponse response = executePostRequest(HTTP_HTTPBIN_ORG_POST, singletonList(nameValue("a", "b")));
+        assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+    }
+
+    @Test
     public void executePostRequestToString_returnsStringContainingRequestArguments() throws Exception {
         assertThat(executePostRequestToString(HTTP_HTTPBIN_ORG_POST, asList(
                 nameValue("aaa", "bbb"),
                 nameValue("ccc", 89456677823452345L))))
                 .contains(HTTP_HTTPBIN_ORG_POST, "aaa", "bbb", "ccc", "89456677823452345");
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void shortPostRequestWithArguments_shouldSuccess() throws Exception {
+        final HttpResponse httpResponse = executePostRequest(HTTP_HTTPBIN_ORG_POST, ImmutableList.of(
+                nameValue("a", "b"),
+                nameValue("hello", 22),
+                nameValue("Fußgängerübergänge", "Пішохідні переходи"),
+                nameValue("Їжачок", "قنفذ"),
+                nameValue("инь-янь", "陰陽")));
+
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatusCode.OK_200);
+
+        String responseString = responseToString(httpResponse);
+        final JsonNode formNode = ofNullable(jsonMapper.readTree(responseString)).map(node -> node.get("form")).orElse(null);
+        assertThat(formNode).isNotNull();
+        assertThat(formNode.isObject()).isTrue();
+        final Map<String, Object> map = jsonMapper.convertValue(formNode, Map.class);
+        assertThat(map).containsOnly(
+                entry("a", "b"),
+                entry("hello", "22"),
+                entry("Fußgängerübergänge", "Пішохідні переходи"),
+                entry("Їжачок", "قنفذ"),
+                entry("инь-янь", "陰陽"));
+    }
+
+    @Test
+    public void shortPostRequestWithoutArguments_shouldSuccess() throws Exception {
+        final HttpResponse httpResponse = executePostRequest(HTTP_HTTPBIN_ORG_POST, null);
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatusCode.OK_200);
     }
 }
