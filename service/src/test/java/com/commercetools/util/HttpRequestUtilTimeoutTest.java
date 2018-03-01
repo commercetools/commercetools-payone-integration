345f99ff35ede55bbfc658243cb7694d26c31313
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2018-03-01 13:28:41 +0100

    #213: fix HttpRequestUtilTimeoutTest to use own mock HTTP service

--- service/src/test/java/com/commercetools/util/HttpRequestUtilTimeoutTest.java
+++ service/src/test/java/com/commercetools/util/HttpRequestUtilTimeoutTest.java
@@ -1,24 +1,112 @@
 package com.commercetools.util;
 
+import com.sun.net.httpserver.HttpServer;
 import io.sphere.sdk.http.HttpStatusCode;
 import org.apache.http.HttpResponse;
-import org.junit.Ignore;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
 
 import static com.commercetools.util.HttpRequestUtil.*;
 import static java.lang.String.format;
+import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIOException;
 
-@Ignore("14.02.2017: slowwly has issues now, so temporary skip the test")
-// should be re-factored: https://github.com/commercetools/commercetools-payone-integration/issues/213
 public class HttpRequestUtilTimeoutTest {
     /**
-     * Url pattern to <a href="http://slowwly.robertomurray.co.uk/">slowwly</a> http mocking resource.
-     * <p>
-     * <b>%d</b> is a placeholder for response delay in msec.
+     * Url pattern to local mocking http service.
+     * <ol>
+     * <li><b>%d</b> is a placeholder for socket port number.</li>
+     * <li><b>%d</b> is a placeholder for response delay in msec.</li>
+     * </ol>
      */
-    private static final String SLOWWLY_URL_PATTERN = "http://slowwly.robertomurray.co.uk/delay/%d/url/http://google.com";
+    private static final String URL_PATTERN = "http://localhost:%d?delay=%d";
+
+    private static Logger LOGGER = LoggerFactory.getLogger(HttpRequestUtilTimeoutTest.class);
+
+    private int testRandomPort;
+
+    /**
+     * Mock http service instance, initiated separately for every test case.
+     *
+     * @see <a href="https://docs.oracle.com/javase/8/docs/jre/api/net/httpserver/spec/com/sun/net/httpserver/HttpServer.html">
+     * HttpServer</a>
+     */
+    private HttpServer server;
+
+
+    /**
+     * Create mock http service which responses after some <i>delay</i>, specified in the the GET query.
+     */
+    @Before
+    public void setUp() {
+        int retries = 3;
+        server = null;
+
+        // run the server on random port
+        do {
+            try {
+                // see https://en.wikipedia.org/wiki/Ephemeral_port#Range for the magic port number values range
+                testRandomPort = 49152 + (new Random(System.currentTimeMillis())).nextInt(65536 - 49152);
+                server = HttpServer.create(new InetSocketAddress(testRandomPort), 0);
+                break;
+            } catch (IOException e) {
+                retries--;
+                LOGGER.warn("Can't create mock http service on port {}, retry {} times...", testRandomPort, retries);
+            }
+        } while (retries > 0);
+
+        if (server == null) {
+            throw new IllegalStateException("Can't create mock http service after retries, last tried port number was " + testRandomPort);
+        }
+
+        // configure the server to "sleep" number of milliseconds specified in "?delay=" query argument
+        server.createContext("/", httpExchange -> {
+            List<NameValuePair> query = URLEncodedUtils.parse(httpExchange.getRequestURI(), StandardCharsets.UTF_8);
+
+            // read value from "?delay=" query argument
+            final long timeout = query.stream().filter(pair -> "delay".equals(pair.getName()))
+                    .map(NameValuePair::getValue)
+                    .mapToLong(Long::decode)
+                    .findFirst()
+                    .orElse(0);
+
+            LOGGER.info("Mock http server makes {} msec delay", timeout);
+
+            try {
+                sleep(timeout);
+            } catch (InterruptedException e) {
+                LOGGER.error("Mock http server sleeping error", e);
+            }
+
+            LOGGER.info("Mock http server continue response after {} msec ", timeout);
+
+            httpExchange.sendResponseHeaders(200, 0);
+            httpExchange.getResponseBody().close();
+        });
+
+        server.start();
+    }
+
+    @After
+    public void tearDown() {
+        if (server != null) {
+            server.removeContext("/");
+            server.stop(1);
+            server = null;
+        }
+    }
 
     /**
      * Verify normal GET response.
@@ -29,7 +117,7 @@ public class HttpRequestUtilTimeoutTest {
     @Test
     public void shortGetRequest_shouldSuccess() throws Exception {
         final int timeout = REQUEST_TIMEOUT / 2;
-        final HttpResponse httpResponse = executeGetRequest(format(SLOWWLY_URL_PATTERN, timeout));
+        final HttpResponse httpResponse = executeGetRequest(format(URL_PATTERN, testRandomPort, timeout));
 
         assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatusCode.OK_200);
     }
@@ -46,7 +134,7 @@ public class HttpRequestUtilTimeoutTest {
         final int minimalFullRequestDuration = REQUEST_TIMEOUT * (RETRY_TIMES + 1);
 
         final long start = System.currentTimeMillis();
-        assertThatIOException().isThrownBy(() -> executeGetRequest(format(SLOWWLY_URL_PATTERN, timeout)));
+        assertThatIOException().isThrownBy(() -> executeGetRequest(format(URL_PATTERN, testRandomPort, timeout)));
         final long executionTime = System.currentTimeMillis() - start;
 
         assertThat(executionTime).isGreaterThanOrEqualTo(minimalFullRequestDuration);
@@ -69,7 +157,7 @@ public class HttpRequestUtilTimeoutTest {
         final int maximalFullRequestDuration = (int) (REQUEST_TIMEOUT * 1.5);
 
         final long start = System.currentTimeMillis();
-        assertThatIOException().isThrownBy(() -> executePostRequest(format(SLOWWLY_URL_PATTERN, timeout), null));
+        assertThatIOException().isThrownBy(() -> executePostRequest(format(URL_PATTERN, testRandomPort, timeout), null));
         final long executionTime = System.currentTimeMillis() - start;
 
         assertThat(executionTime).isLessThan(maximalFullRequestDuration);
