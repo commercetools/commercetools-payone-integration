f2a28ebfa195c979aa7b3478b7c2d83abff022c7
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2018-04-03 17:59:23 +0200

    #213: remove "confusings"

--- service/src/main/java/com/commercetools/pspadapter/payone/transaction/paymentinadvance/BankTransferInAdvanceChargeTransactionExecutor.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/transaction/paymentinadvance/BankTransferInAdvanceChargeTransactionExecutor.java
@@ -21,8 +21,7 @@ import static io.sphere.sdk.payments.TransactionType.CHARGE;
  *     Prepayment   Not supported by this request!
  * </pre>
  * <p>
- * Looks like it is accepted by Payone service, but might be switched off in the future any time, also some logistic
- * companies might have issues handling these transaction types for this method.
+ * Looks like it is accepted by Payone service, but might be switched off in the future any time.
  */
 public class BankTransferInAdvanceChargeTransactionExecutor extends BaseBankTransferInAdvanceTransactionExecutor {
 
