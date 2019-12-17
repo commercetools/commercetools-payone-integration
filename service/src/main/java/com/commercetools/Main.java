47b7e4c73938821b575be352ed329a432f169292
Hesham Massoud <hesham.massoud@commercetools.de>
2019-12-17 23:34:25 +0100

    #240: Add SLF4J bridge.

--- service/src/main/java/com/commercetools/Main.java
+++ service/src/main/java/com/commercetools/Main.java
@@ -7,6 +7,7 @@ import org.quartz.CronScheduleBuilder;
 import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import static java.lang.String.format;
 
@@ -26,6 +27,7 @@ public class Main {
      * @throws SchedulerException if scheduled quartz jobs can't be started
      */
     public static void main(String[] args) throws SchedulerException {
+        bridgeJULToSLF4J();
 
         final PropertyProvider propertyProvider = new PropertyProvider();
         final ServiceConfig serviceConfig = new ServiceConfig(propertyProvider);
@@ -50,5 +52,22 @@ public class Main {
                 CronScheduleBuilder.cronSchedule(serviceConfig.getScheduledJobCronForLongTimeFramePoll()),
                 ScheduledJobLongTimeframe.class,
                 integrationService);
+    }
+
+    /**
+     * Routes all incoming j.u.l. (java.util.logging.Logger) records to the SLF4j API. This is done by:
+     * <ol>
+     *     <li>Removing existing handlers attached to the j.u.l root logger.</li>
+     *     <li>Adding SLF4JBridgeHandler to j.u.l's root logger.</li>
+     * </ol>
+     * <p>Why we do the routing?
+     * <p>Some dependencies (e.g. org.javamoney.moneta's DefaultMonetaryContextFactory) log events using the
+     * j.u.l. This causes such logs to ignore the logback.xml configuration which is only
+     * applied to logs from the SLF4j implementation.
+     *
+     */
+    private static void bridgeJULToSLF4J() {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
     }
 }
