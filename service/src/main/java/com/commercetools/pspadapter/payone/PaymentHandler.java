a49d0f5a3fe70a6acd59af910d7cbf856a75a12f
Hesham Massoud <hesham.massoud@commercetools.de>
2019-12-20 18:39:28 +0100

    Improve error handeling.

--- service/src/main/java/com/commercetools/pspadapter/payone/PaymentHandler.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/PaymentHandler.java
@@ -52,15 +52,19 @@ public class PaymentHandler {
      */
     public PaymentHandleResult handlePayment(@Nonnull final String paymentId) {
         try {
-            ConcurrentModificationException lastConcurrentModificationException = null;
             for (int i = 0; i < RETRIES_LIMIT; i++) {
                 try {
-                    final PaymentWithCartLike paymentWithCartLike = commercetoolsQueryExecutor
-                        .getPaymentWithCartLike(paymentId);
-                    String paymentInterface = paymentWithCartLike.getPayment().getPaymentMethodInfo()
-                                                                 .getPaymentInterface();
+                    final PaymentWithCartLike paymentWithCartLike =
+                        commercetoolsQueryExecutor.getPaymentWithCartLike(paymentId);
+
+                    final String paymentInterface = paymentWithCartLike
+                        .getPayment()
+                        .getPaymentMethodInfo()
+                        .getPaymentInterface();
+
                     if (!payoneInterfaceName.equals(paymentInterface)) {
-                        logger.warn(tenantNameKeyValue, "Wrong payment interface name: expected [{}], found [{}]",
+                        logger.warn(tenantNameKeyValue,
+                            "Wrong payment interface name: expected [{}], found [{}]",
                             payoneInterfaceName, paymentInterface);
                         return new PaymentHandleResult(HttpStatusCode.BAD_REQUEST_400);
                     }
@@ -68,14 +72,14 @@ public class PaymentHandler {
                     paymentDispatcher.dispatchPayment(paymentWithCartLike);
                     return new PaymentHandleResult(HttpStatusCode.OK_200);
                 } catch (final ConcurrentModificationException concurrentModificationException) {
-                    lastConcurrentModificationException = concurrentModificationException;
+                    if (i == RETRIES_LIMIT - 1) {
+                        return handleConcurrentModificationException(paymentId, concurrentModificationException);
+                    }
                     Thread.sleep(RETRY_DELAY);
                 }
             }
-            throw lastConcurrentModificationException;
-        }
-        catch (final ConcurrentModificationException concurrentModificationException) {
-            return handleConcurrentModificationException(paymentId, concurrentModificationException);
+            throw new IllegalStateException("Unexpected logical path. This line should be unreachable as long as "
+                + "RETRIES_LIMIT > 0");
         } catch (final NotFoundException | NoCartLikeFoundException e) {
             return handleNotFoundException(paymentId, e);
         } catch (final ErrorResponseException e) {
@@ -95,13 +99,19 @@ public class PaymentHandler {
         return new PaymentHandleResult(HttpStatusCode.ACCEPTED_202, errorMessage);
     }
 
-    private PaymentHandleResult handleNotFoundException(@Nonnull String paymentId, @Nonnull Exception exception ) {
+    private PaymentHandleResult handleNotFoundException(
+        @Nonnull final String paymentId,
+        @Nonnull final Exception exception ) {
+
         final String body = format("Failed to process the commercetools Payment with id [%s], as the payment or the cart could not be found.", paymentId);
         logger.error(tenantNameKeyValue, body, exception);
         return new PaymentHandleResult(HttpStatusCode.NOT_FOUND_404, body);
     }
 
-    private PaymentHandleResult errorResponseHandler(@Nonnull ErrorResponseException e, @Nonnull String paymentId) {
+    private PaymentHandleResult errorResponseHandler(
+        @Nonnull final ErrorResponseException e,
+        @Nonnull final String paymentId) {
+
         logger.error(tenantNameKeyValue,
             format("Failed to process the commercetools Payment with id [%s] due to an error response from the commercetools platform.", paymentId), e);
         return new PaymentHandleResult(e.getStatusCode(),
@@ -109,9 +119,12 @@ public class PaymentHandler {
                     + "commercetools platform. Try again later.", paymentId));
     }
 
-    private PaymentHandleResult handleException(@Nonnull Exception throwable, @Nonnull String paymentId) {
+    private PaymentHandleResult handleException(
+        @Nonnull final Exception exception,
+        @Nonnull final String paymentId) {
+
         logger.error(tenantNameKeyValue,
-            format("Unexpected error occurred when processing commercetools Payment with id [%s].", paymentId), throwable);
+            format("Unexpected error occurred when processing commercetools Payment with id [%s].", paymentId), exception);
         return new PaymentHandleResult(INTERNAL_SERVER_ERROR_500,
                 format("Unexpected error occurred when processing commercetools Payment with id [%s]. "
                     + "See the service logs", paymentId));
