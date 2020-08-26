0e435d764214e82e0300a69675e6e5855ad0ee56
King-Hin Leung <kinghin.leung@gmail.com>
2020-08-26 12:42:23 +0200

    Implement unit test for retry mechanism

--- service/src/test/java/com/commercetools/util/ClientConfigurationUtilTest.java
+++ service/src/test/java/com/commercetools/util/ClientConfigurationUtilTest.java
@@ -5,6 +5,12 @@ import com.commercetools.pspadapter.tenant.TenantConfig;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
+import io.sphere.sdk.client.TestDoubleSphereClientFactory;
+import io.sphere.sdk.http.HttpResponse;
+import io.sphere.sdk.http.HttpStatusCode;
+import io.sphere.sdk.taxcategories.queries.TaxCategoryQuery;
+import io.sphere.sdk.taxcategories.queries.TaxCategoryQueryBuilder;
+import java.util.concurrent.CompletionException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +18,9 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -32,5 +41,44 @@ public class ClientConfigurationUtilTest {
         assertThat(sphereClient instanceof BlockingSphereClient).isTrue();
     }
 
+    @Test
+    public void decorateClient_shouldRetry_when5xxHttpResponse() {
+        SphereClient mockSphereUnderlyingClient =
+            spy(TestDoubleSphereClientFactory.createHttpTestDouble(
+                intent -> HttpResponse.of(HttpStatusCode.BAD_GATEWAY_502)));
 
+        final BlockingSphereClient blockingSphereClient =
+            ClientConfigurationUtil.decorateSphereClient(mockSphereUnderlyingClient);
+
+        final TaxCategoryQuery query = TaxCategoryQueryBuilder.of().build();
+        try {
+            blockingSphereClient.execute(query).toCompletableFuture().join();
+        } catch (CompletionException e) {
+            // Skipped
+        }
+        verify(mockSphereUnderlyingClient,
+                times(ClientConfigurationUtil.RETRIES_LIMIT+1))
+                .execute(query);
+    }
+
+
+    @Test
+    public void decorateClient_shouldNotRetry_when4xxHttpResponse() {
+        SphereClient mockSphereUnderlyingClient =
+                spy(TestDoubleSphereClientFactory.createHttpTestDouble(
+                        intent -> HttpResponse.of(HttpStatusCode.BAD_REQUEST_400)));
+
+        final BlockingSphereClient blockingSphereClient =
+                ClientConfigurationUtil.decorateSphereClient(mockSphereUnderlyingClient);
+
+        final TaxCategoryQuery query = TaxCategoryQueryBuilder.of().build();
+        try {
+            blockingSphereClient.execute(query).toCompletableFuture().join();
+        } catch (CompletionException e) {
+            // Skipped
+        }
+        verify(mockSphereUnderlyingClient,
+                times(1))
+                .execute(query);
+    }
 }
