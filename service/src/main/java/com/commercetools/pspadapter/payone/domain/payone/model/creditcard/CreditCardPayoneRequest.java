d6ca4497aadcb755e4ab96816d217f46341cba8a
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2022-01-04 14:13:23 +0100

    Remove unneeded class.

--- service/src/main/java/com/commercetools/pspadapter/payone/domain/payone/model/creditcard/CreditCardPayoneRequest.java
+++ service/src/main/java/com/commercetools/pspadapter/payone/domain/payone/model/creditcard/CreditCardPayoneRequest.java
@@ -2,7 +2,6 @@ package com.commercetools.pspadapter.payone.domain.payone.model.creditcard;
 
 import com.commercetools.pspadapter.payone.config.PayoneConfig;
 import com.commercetools.pspadapter.payone.domain.ctp.PaymentWithCartLike;
-import com.commercetools.pspadapter.payone.domain.payone.model.common.AuthorizationRequestWithCart;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.ClearingType;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.PayoneRequestWithCart;
 import com.commercetools.pspadapter.payone.domain.payone.model.common.RequestType;
@@ -11,7 +10,7 @@ import com.commercetools.pspadapter.payone.domain.payone.model.common.RequestTyp
  * @author fhaertig
  * @since 18.01.16
  */
-public class CreditCardPayoneRequest extends AuthorizationRequestWithCart {
+public class CreditCardPayoneRequest extends PayoneRequestWithCart {
 
     private String pseudocardpan;
 
